3121ed7548bab62e2e3bf0e7056b2064755d1312
miguedonatien <miguedonatien1@gmail.com>
2021-02-04 05:52:36 -0700

    change directory app/ to dis/

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(express.static(__dirname + "/app"));
+app.use(express.static(__dirname + "/dist"));
 app.listen(port);
